40ad71cba6d75cd3f008f7aa205e5d08f4807b5f
Hugo Hum <hugohum@Hugos-MBP-2.cogeco.local>
2022-01-01 14:07:18 -0500

    first commit

---
+++ shopify.config.js
@@ -0,0 +1,6 @@
+export default {
+  locale: 'en-us',
+  storeDomain: 'testingroundsforhint.myshopify.com',
+  storefrontToken: '31cb4a42aab796885f53c7b4cbadfc2c',
+  graphqlApiVersion: 'unstable',
+};
